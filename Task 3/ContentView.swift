ac47ab4ff7259c8b6e15da7b5dbd11b43b7ba8aa
OgabekDev <ogabekdev@gmail.com>
2022-11-26 13:10:34 +0500

    All Done

--- Task 3/ContentView.swift
+++ Task 3/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            Image("img_1")
+                .resizable()
+                .scaledToFill()
+            
+            Image("img_2")
+                .resizable()
+                .scaledToFill()
+            
+            Image("img_3")
+                .resizable()
+                .scaledToFill()
         }
-        .padding()
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
